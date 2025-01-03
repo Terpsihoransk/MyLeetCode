5c0f63ffdf4cae610d0477d2a6d648a8ff78d731
arsenevaea <arsenjeva.ea@gmail.com>
2025-01-03 15:30:39 +0700

    Palindromes

--- src/EasyLevel/PalindromeNumber.java
+++ src/EasyLevel/PalindromeNumber.java
@@ -30,13 +30,13 @@ public class PalindromeNumber {
     public static boolean isPalindromeFaster(int x) {
         if (x < 0)
             return false;
-        int temp = x;
-        int reversed = 0;
-        while (temp != 0) {
-            reversed = (reversed * 10) + (temp % 10);
-            temp /= 10;
+        int originalNumber = x;
+        int reversedNumber = 0;
+        while (x > 0) {
+            reversedNumber = (reversedNumber * 10) + (x % 10);
+            x = x / 10;
         }
-        return reversed == x;
+        return reversedNumber == originalNumber;
     }
 
     // Можно решить через приведение к строке
@@ -48,7 +48,10 @@ public class PalindromeNumber {
 
     // Чистое определение является ли строка палиндромом
     private static boolean isTextPalindrome(String word) {
-        var chars = word.toLowerCase().replaceAll("\\s", "").toCharArray();
+        var chars = word
+                .toLowerCase()
+                .replaceAll("\\s", "")
+                .toCharArray();
         var left = 0;
         var right = chars.length - 1;
         while (left < right) {
@@ -60,4 +63,16 @@ public class PalindromeNumber {
         }
         return true;
     }
+    /**
+     * Преобразование в строку String.valueOf(x): Эта операция зависит от количества цифр в числе x.
+     * В худшем случае (для очень больших чисел) ее сложность можно считать как O(log10(n)),
+     * где n - значение числа. Однако, эта операция выполняется за константу времени.
+     * toLowerCase() и replaceAll("\\s", ""): Эти операции работают со строкой, сложность этих операций зависит от длины строки,
+     * но в нашем случае длина строки пропорциональна количеству цифр в исходном числе, поэтому это также O(log10(n)).
+     * replaceAll может быть несколько медленнее из-за использования регулярных выражений, но порядок сложности остается тем же.
+     * toCharArray(): Эта операция также зависит от длины строки, а значит, и от количества цифр в числе, и имеет сложность O(log10(n)).
+     * Цикл while (left < right): В этом цикле мы сравниваем символы с левого и правого края строки.
+     * В худшем случае цикл выполняется примерно n/2 раз, где n — количество символов в строке.
+     * Поэтому, асимптотическая сложность цикла — O(n). Но так как n пропорционально количеству цифр в числе, то это можно также считать как O(log10(n)).
+     */
 }
